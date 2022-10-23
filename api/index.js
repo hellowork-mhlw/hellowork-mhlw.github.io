8f39c5698b9595eb17314b204299a50b6715b519
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2022-10-23 11:36:42 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,15 @@
 import fetch from 'node-fetch';
 
 export default function handler(req, res) {
-  fetch('http://httpbin.org/ip').then(r => r.body.pipe(res))
+  fetch("https://www.hellowork.mhlw.go.jp/kensaku/GECA110010.do", {
+    method: "POST",
+    body: new URLSearchParams({
+      kjKbnRadioBtn: 1,
+      searchBtn: '',
+      screenId: 'GECA110010',
+      maba_vrbs: 'searchBtn',
+      fwListNaviDisp: 50,
+      ...req.query
+    }),
+  }).then(r => r.body.pipe(res))
 }
