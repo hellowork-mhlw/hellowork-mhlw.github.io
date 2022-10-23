9621ba756191bdd79c4813fd625a4ff23587e257
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2022-10-23 12:04:21 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,5 @@
 import fetch from 'node-fetch';
+import { JSDOM } from "jsdom";
 
 export default function handler(req, res) {
   fetch("https://www.hellowork.mhlw.go.jp/kensaku/GECA110010.do", {
@@ -11,5 +12,5 @@ export default function handler(req, res) {
       fwListNaviDisp: 50,
       ...req.query
     }),
-  }).then(r => r.body.pipe(res))
+  }).then(r => r.text()).then(html => res.json([...new JSDOM(html).window.document.querySelectorAll('.kyujin')].map(k=>k.querySelector('.m13>div').textContent)))
 }
