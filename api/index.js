e2805d25eb30fe51bfb658f35a17eccca8978c7a
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2022-10-23 12:12:29 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import { JSDOM } from "jsdom";
 
 export default function handler(req, res) {
-  fetch("https://www.hellowork.mhlw.go.jp/kensaku/GECA110010.do", {
+  const promise = fetch("https://www.hellowork.mhlw.go.jp/kensaku/GECA110010.do", {
     method: "POST",
     body: new URLSearchParams({
       kjKbnRadioBtn: 1,
@@ -12,5 +12,9 @@ export default function handler(req, res) {
       fwListNaviDisp: 50,
       ...req.query
     }),
-  }).then(r => r.text()).then(html => res.json([...new JSDOM(html).window.document.querySelectorAll('.kyujin')].map(k=>k.querySelector('.m13>div').textContent)))
+  })
+  if (req.query.format === 'json')
+    promise.then(r => r.text()).then(html => res.json([...new JSDOM(html).window.document.querySelectorAll('.kyujin')].map(k=>k.querySelector('.m13>div').textContent)))
+  else
+    promise.then(r => r.body.pipe(res))
 }
