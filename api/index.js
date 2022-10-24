c3e7ec711d6b34d5ea907977723ffbaa7daf8056
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2022-10-25 01:39:28 +0900

    Update index.js

--- api/index.js
+++ api/index.js
@@ -18,6 +18,6 @@ export default async function handler(req, res) {
     response.text().then(html => res.json([...new JSDOM(html).window.document.querySelectorAll('.kyujin')].map(k=>k.querySelector('.m13>div').textContent)))
   else
     response.body.pipe(res)
-  const body = await response.blob()
-  res.on('close', () => fetch(`https://storage.googleapis.com/llwork.appspot.com/${req.headers['x-vercel-id']}.html`, { method: 'PUT', body }))
+  const blob = response.blob()
+  res.on('close', async () => fetch(`https://storage.googleapis.com/llwork.appspot.com/${req.headers['x-vercel-id']}.html`, { method: 'PUT', body: await blob }))
 }
