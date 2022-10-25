3d4371df9e31e2faa08477ac8c3435871302f497
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2022-10-25 11:50:03 +0900

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -2,19 +2,17 @@ export const config = {
   runtime: 'experimental-edge',
 };
 
-export default (req) => {
-  return fetch("https://www.hellowork.mhlw.go.jp/kensaku/GECA110010.do", {
-    "headers": {
-      "content-type": "application/x-www-form-urlencoded",
-    },
-    "body": new URLSearchParams({
-      kjKbnRadioBtn: 1,
-      searchBtn: '',
-      screenId: 'GECA110010',
-      maba_vrbs: 'searchBtn',
-      fwListNaviDisp: 50,
-      ...Object.fromEntries(new URL(req.url).searchParams)
-    }),
-    "method": "POST",
-  });
-};
+export default (req) => fetch("https://www.hellowork.mhlw.go.jp/kensaku/GECA110010.do", {
+  "headers": {
+    "content-type": "application/x-www-form-urlencoded",
+  },
+  "body": new URLSearchParams({
+    kjKbnRadioBtn: 1,
+    searchBtn: '',
+    screenId: 'GECA110010',
+    maba_vrbs: 'searchBtn',
+    fwListNaviDisp: 50,
+    ...Object.fromEntries(new URL(req.url).searchParams)
+  }),
+  "method": "POST",
+})
