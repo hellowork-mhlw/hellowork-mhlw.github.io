63b94b9913f9369991368e1aa55f13168b5b5e5f
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2022-10-25 10:55:00 +0900

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -5,12 +5,6 @@ export const config = {
 export default (req) => {
   console.log(req.url);
   return fetch("https://www.hellowork.mhlw.go.jp/kensaku/GECA110010.do", {
-    "headers": {
-      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "accept-language": "ja,en-US;q=0.9,en;q=0.8",
-      "cache-control": "max-age=0",
-      "content-type": "application/x-www-form-urlencoded",
-    },
     "body": new URLSearchParams({
       kjKbnRadioBtn: 1,
       searchBtn: '',
