5a416bbdaa7819bde4553a4f37963e3abec83b9f
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2022-11-07 19:46:03 +0900

    Update github.js

--- api/github.js
+++ api/github.js
@@ -3,4 +3,4 @@ export const config = {
 };
 
 // curl https://hellowork-mhlw.vercel.app/api/github?url=https://github.com/git/git
-export default (req) => fetch(new URL(req.url).searchParams.get('url') + '/info/refs?service=git-upload-pack').then(r => new Response(r.status === 200, { headers: { "content-type": "application/json; charset=utf-8", 'Cache-Control': 's-maxage=2678400' } })).catch(r => new Response(false, { headers: { "content-type": "application/json; charset=utf-8", 'Cache-Control': 's-maxage=2678400' } }))
+export default (req) => fetch(new URL(req.url).searchParams.get('url') + '/info/refs?service=git-upload-pack').then(r => new Response(r.status === 200, { headers: { "content-type": "application/json; charset=utf-8", 'Cache-Control': 's-maxage=2678400' } }))
