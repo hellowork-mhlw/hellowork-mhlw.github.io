89b586424f8cf647d18e1884ae0a24b3b55f66d5
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2022-11-07 18:38:11 +0900

    Update github.js

--- api/github.js
+++ api/github.js
@@ -2,4 +2,4 @@ export const config = {
   runtime: 'experimental-edge',
 };
 
-export default (req) => (console.log(JSON.stringify(req, null, 2)), fetch('https://github.com/git/git/info/refs?service=git-upload-pack').then(r => new Response(r.status === 200, { headers: { "content-type": "application/json; charset=utf-8", 'Cache-Control': 's-maxage=2678400' } })))
+export default (req) => (console.log(req.url), fetch('https://github.com/git/git/info/refs?service=git-upload-pack').then(r => new Response(r.status === 200, { headers: { "content-type": "application/json; charset=utf-8", 'Cache-Control': 's-maxage=2678400' } })))
