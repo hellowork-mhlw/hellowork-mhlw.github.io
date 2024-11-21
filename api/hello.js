f3e8c2e519079c089a6be9f4817cba4809d840c2
hellowork-mhlw <116468267+hellowork-mhlw@users.noreply.github.com>
2024-11-21 21:29:35 +0900

    Update hello.js

--- api/hello.js
+++ api/hello.js
@@ -2,5 +2,5 @@ import { simpleParser } from 'mailparser';
 import Imap from 'imap';
 
 export function GET(request) {
-  return new Response(`Hello from ${process.env.VERCEL_REGION}`);
+  return new Response(`Hello2 from ${process.env.VERCEL_REGION}`);
 }
